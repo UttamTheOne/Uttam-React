7b2349e1d0ea4b6fdc0153d83aecab1bb4be863b
Uttam Deshani <uttamdeshani@gmail.com>
2020-06-05 13:09:37 +0530

    User Component to Display User Information

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import User from './Components/User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div style={{ border: '2px dotted black', margin: '20px 200px', color: 'black', borderRadius: '25px' }}>
+          <h2>Users Info</h2>
+          <hr />
+          <User FirstName="Uttam" LastName="Deshani" City='Vadodara' State='Gujarat' Country="India" />
+          <User FirstName="Mihir" LastName="Shah" City='Ahmedabad' State='Gujarat' Country="India" />
+          <User FirstName="Aneri" LastName="Patel" City='Surat' State='Gujarat' Country="India" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,47 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-const numVar = 8;
-var text = <h1>Hello World!</h1>;
-let users = [
-	{
-		"firstname": "Uttam",
-		"lastname": "Deshani"
-	},
-	{
-		"firstname": "Mihir",
-		"lastname": "Shah"
-	}
-];
-
-const user1 = () => { return (<h3>Hello, {users[0].firstname} {users[0].lastname} {numVar} </h3>); }
-const user2 = <h3>Hello, {users[1].firstname} {users[1].lastname} </h3>
-
-function printUser() {
-	return (<div>
-		{text}
-		{user1()}
-		{user2}
-	</div>);
-}
-
-function tick(){
-
-	const element = (
-		<div>
-				<h2>Timer : {new Date().toLocaleTimeString()}</h2>
-				{printUser()}		
-		</div>
-	);
-ReactDOM.render(element,
+ReactDOM.render(<App/>,
 	document.getElementById('root')
 );
-}
-
-setInterval(tick,1000);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
