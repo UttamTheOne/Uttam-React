035282ebe360408d9a94bbdce4feef64b295e514
Uttam Deshani <uttam.deshani@theonetechnologies.co.in>
2020-06-23 23:20:42 +0530

    Updates

---
+++ precache-manifest.a83dab98d9e904e462b7223c65dbcec4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1160f2cc0138046e70b804a6a864a3f6",
+    "url": "/Uttam-React/index.html"
+  },
+  {
+    "revision": "96c8ce13747b231fd411",
+    "url": "/Uttam-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6c0dc4009c81eb3489f6",
+    "url": "/Uttam-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96c8ce13747b231fd411",
+    "url": "/Uttam-React/static/js/2.53521e77.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Uttam-React/static/js/2.53521e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c0dc4009c81eb3489f6",
+    "url": "/Uttam-React/static/js/main.614c5b5c.chunk.js"
+  },
+  {
+    "revision": "03c157979f51e436f4df",
+    "url": "/Uttam-React/static/js/runtime-main.09c63ec4.js"
+  },
+  {
+    "revision": "ab9d9b960fa5563cb1431ab42a811e52",
+    "url": "/Uttam-React/static/media/Desktop.ab9d9b96.png"
+  },
+  {
+    "revision": "c0f804cf9991bad3dddbbe546716df6f",
+    "url": "/Uttam-React/static/media/ExtraSmall.c0f804cf.jpg"
+  },
+  {
+    "revision": "cac2084b462bce2fb75838ff10c09c8a",
+    "url": "/Uttam-React/static/media/Laptop.cac2084b.jpg"
+  },
+  {
+    "revision": "a07cac092364229b27bc8d97b48499dd",
+    "url": "/Uttam-React/static/media/Smartphone.a07cac09.jpg"
+  },
+  {
+    "revision": "1a7d01db2feb928c5f8f591ffb48d6b4",
+    "url": "/Uttam-React/static/media/Tablet.1a7d01db.jpg"
+  }
+]);
